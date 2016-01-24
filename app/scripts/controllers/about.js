713491bd7d16a003fdc399d39eb5d616198557fe
Ausin Formica <austin.formica@gmail.com>
2016-01-24 15:25:11 -0500

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nudgerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nudgerApp
+ */
+angular.module('nudgerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nudgerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nudgerApp
+ */
+angular.module('nudgerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
