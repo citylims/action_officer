446c7f4a91811a2d4b96e19407b771c5c421aa3a
Ausin Formica <austin.formica@gmail.com>
2016-01-27 16:51:48 -0500

    asteroid tracker

--- app/scripts/services/nasaservice.js
+++ app/scripts/services/nasaservice.js
@@ -8,9 +8,11 @@
  * Service in the nudgerApp.
  */
 angular.module('nudgerApp')
-  .service('NasaService', function ($http) {
+  .service('NasaService', function ($http, AIService) {
     var apiUrl = 'https://api.nasa.gov';
     var key = 'Iwko8aFjq9Mw7nQL4Ou7cVEPCvByXKt9kGafWAwD';
+    var formattedDate = AIService.formattedDate();
+
 
     function mars() {
       var query = '/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=' + key;
@@ -19,8 +21,28 @@ angular.module('nudgerApp')
       });
     }
 
+    function asteroids() {
+      var dates = processDate(formattedDate);
+      var query = '/neo/rest/v1/feed?start_date=' + dates[0] + '&end_date=' + dates[1] +'&api_key=' + key;
+      return $http.get(apiUrl + query).then(function(res) {
+        return res.data;
+      });
+    }
+
+    function processDate(date){
+      var arr = [];
+      var parts = date.split('/');
+      var day = parseInt(parts[1]);
+      var month = parts[0];
+      var year = parts[2];
+      arr[0] = year + '-' + month + '-' + (day - 1);
+      arr[1] = year + '-' + month + '-' + day;
+      return arr;
+    }
+
     return {
-      mars: mars
+      mars: mars,
+      asteroids: asteroids
     };
 
   });
